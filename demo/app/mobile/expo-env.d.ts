d8ddacd8bfebe78af17a7c5df538b0a3f273e0c9
pedrobzz <57912928+pedrobzz@users.noreply.github.com>
2026-07-16 21:25:51 -0300

    chore(demo): add .gitignore for expo-env.d.ts to prevent edits

--- demo/app/mobile/expo-env.d.ts
+++ demo/app/mobile/expo-env.d.ts
@@ -1,7 +1,3 @@
 /// <reference types="expo/types" />
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly EXPO_PUBLIC_DBZZ_URL?: string;
-  }
-}
+// NOTE: This file should not be edited and should be in your git ignore
